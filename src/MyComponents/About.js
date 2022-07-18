b1928345c3a30a9acc89d1bb5345c9c271c4b3b1
gavinspet <kartykjdj@gmail.com>
2022-07-18 19:17:41 +0530

    fdghj

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <h1>About Page</h1>
+            <p>This is about page of my reactjs todo app.
+                Also, hi my name is Kartick!!
+            </p>
+        </div>
+    )
+}
